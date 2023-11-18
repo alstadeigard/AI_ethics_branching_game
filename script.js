ec3b2d939b6a8f5d06d0c6748a0c9cea3bf7d676
Eigard Oernsrud Alstad <E.O.Alstad@student.tudelft.nl>
2023-11-18 03:52:25 +0100

    Sample games
    Added two sample games

--- script.js
+++ script.js
@@ -1,8 +1,60 @@
-function chooseOption(option) {
-    var resultText = document.getElementById('resultText');
-    if (option === 1) {
-        resultText.innerHTML = "You chose Option 1. [Outcome of Option 1]";
+document.addEventListener('DOMContentLoaded', function() {
+    loadGameData();
+});
+
+function loadGameData(filePath) {
+    fetch(filePath)
+        .then(response => response.json())
+        .then(data => startGame(data.scenarios))
+        .catch(error => console.error('Error loading game data:', error));
+}
+
+function startGame(scenarios) {
+    displayQuestion(scenarios, scenarios[0].id);
+}
+
+function displayQuestion(scenarios, questionId) {
+    const question = scenarios.find(scenario => scenario.id === questionId);
+    if (!question) return;
+
+    document.getElementById('questionText').textContent = question.text;
+    const choicesContainer = document.getElementById('choicesContainer');
+    choicesContainer.innerHTML = '';
+
+    question.choices.forEach(choice => {
+        const button = document.createElement('button');
+        button.textContent = choice.text;
+        button.onclick = () => handleChoice(scenarios, choice, question);
+        choicesContainer.appendChild(button);
+    });
+
+    // Remove any existing next question button
+    const nextButton = document.getElementById('nextButton');
+    if (nextButton) {
+        nextButton.remove();
+    }
+}
+
+function handleChoice(scenarios, choice, currentQuestion) {
+    document.getElementById('resultText').textContent = choice.outcome;
+
+    // Disable all choice buttons
+    const buttons = document.querySelectorAll('#choicesContainer button');
+    buttons.forEach(button => button.disabled = true);
+
+    // Create and display 'Next Question' button
+    const nextButton = document.createElement('button');
+    nextButton.textContent = 'Next Question';
+    nextButton.id = 'nextButton';
+    nextButton.onclick = () => displayNextQuestion(scenarios, choice.next);
+    document.getElementById('gameArea').appendChild(nextButton);
+}
+
+function displayNextQuestion(scenarios, nextQuestionId) {
+    if (nextQuestionId) {
+        displayQuestion(scenarios, nextQuestionId);
     } else {
-        resultText.innerHTML = "You chose Option 2. [Outcome of Option 2]";
+        // End of game or next steps
+        document.getElementById('resultText').textContent = 'End of the game.';
     }
 }
